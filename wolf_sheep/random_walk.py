093684c18451a35799aba541dc43073297d02fd4
Hossein <farhadiasghar125@gmail.com>
2023-12-24 22:49:50 +0330

    Fix: Change comment class RandomWalk

--- wolf_sheep/random_walk.py
+++ wolf_sheep/random_walk.py
@@ -8,9 +8,7 @@ import mesa
 class RandomWalker(mesa.Agent):
     """
     Class implementing random walker methods in a generalized manner.
-
-    Not intended to be used on its own, but to inherit its methods to multiple
-    other agents.
+    For implement Agents: Wolf and Sheep
     """
 
     grid = None
